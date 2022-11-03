90f68a4e75109a3c666ef671848605c804afb8bc
Luca Milanese <luca.milanese90@gmail.com>
2022-11-03 17:32:59 +0100

    feat: update featured asset list

--- address.config.js
+++ address.config.js
@@ -7,8 +7,8 @@ module.exports = {
     {
       title: 'Text analysis',
       assets: [
-        'did:op:7cb4Fd9E37061393A548809891805c70Af8d044F',
-        'did:op:06176ab87e413c4EAFfE4B7F04F063309ba5e1cf'
+        'did:op:5A3E99a1d126F2546589bCF5f6cB541ab87Aad83',
+        'did:op:392C318F37BD3eAd42501CdeC962D81c3bE03B9B'
       ]
     }
   ]
