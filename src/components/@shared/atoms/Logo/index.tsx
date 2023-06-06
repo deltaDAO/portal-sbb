051d5345cfffb8979790a6d813c9ae5d71dc6428
Luca Milanese <luca.milanese90@gmail.com>
2023-06-06 12:14:37 +0200

    feat: update brand logo

--- src/components/@shared/atoms/Logo/index.tsx
+++ src/components/@shared/atoms/Logo/index.tsx
@@ -1,7 +1,6 @@
 import React, { ReactElement } from 'react'
-import LogoAsset from '@images/brand-logo.svg'
 import styles from './index.module.css'
 
 export default function Logo(): ReactElement {
-  return <LogoAsset className={styles.logo} />
+  return <img className={styles.logo} src="/images/brand-logo.png" />
 }
