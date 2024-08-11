62b902f3f72595e6932e7bbad770f9254d1dcbd2
Kai Meinke <59994024+kaimeinke@users.noreply.github.com>
2024-08-11 17:02:41 +0200

    Update Landing Page

--- address.config.js
+++ address.config.js
@@ -19,23 +19,10 @@ module.exports = {
     {
       title: 'Advanced Use Cases for Text and Data Mining',
       assets: [
-        'did:op:d64b559e6734f95752a7c2ea51b52eb56c9f7ebabc3cd730202f37765a2b661c', // CrossAsia Probability Calculation Data for Topics in Time - State Library of Berlin
-        'did:op:5b350952dfd31393bc505ea0b016c804dae120cc9bf987dcaa31b52fc841f382', // CrossAsia Probability Calculation Algorithm for Topics in Time - State Library of Berlin
-        'did:op:106311fbde5c1f3935bfe514e4cda70437e525a6855f8673023cd35ab681f985', // CrossAsia Text Analysis - State Library of Berlin
-        'did:op:d3eac31ffa9ef6da22ca2517fa21dc1104977cca9d492cf6d137fa4610affb5a' // CrossAsia Text Analysis Algorithm - State Library of Berlin
-      ]
-    },
-    {
-      title: 'OCR',
-      assets: [
-        'did:op:2add32a20d0ee146c51424907c1a4397f50d985cb88f0738c79cc5602e2b70a7', // Eines deutschen Matrosen Nordpolarfahrten : Wilhelm Nindemanns Erinnerungen an die Nordpolexpedition der "Polaris" und "Jeanette"
-        'did:op:99d0a96b16a0218c1b24c8109c6f99c6e2dda0fd96df6a749ea8a010e0d7f755', // Illustrierte Naturgeschichte der drei Reiche
-        'did:op:f231ad11244f2c2e86d694a24765d49dccdaddf1b48b4b0b74ebd586a44bac39', // Die Handelswege und Verkehrsmittel der Gegenwart : ein Leitfaden zur Ergänzung geographischer Lehrbücher für den Schul- und Selbstunterricht
-        'did:op:5ee866c801d9da05fadb375468fbcd6e404d48abbc78e0122f37fbf1d6a3f8e2', // Der Umgang in und mit der Gesellschaft : Ein Handbuch des guten Tons
-        'did:op:a166604b6272ef415e7d980052ada12cff5c9fa2419c12ad6ddbfde19bdc2410', // Forschungsreise in den Molukken und in Borneo
-        'did:op:6feff2740b1a1d07b017348865df62b0568dcd0aaccd29aa571e166fdd422fbe', // Entdeckungsreisen im Wald und auf der Heide : mit seinen lieben jungen Freunden und Freundinnen unternommen
-        'did:op:b6e20571d7b403895aed65e0d5156e4a1dad809c969c38c6fc2154bb26c33d12', // Stanley's sämtliche Reisen in Afrika : von der Auffindung Livingstone's bis zum Entsatze Emin Pascha's
-        'did:op:9226f77fff1c674f65ecbe71d718332454efe5ca3ce9076d5bc3eef5db67a24c' // Jahresbericht über die Nordtirolische Kapuziner-Mission von Bettiah und Nepal (in Vorderindien) : unter d. Schutze d. heiligen Fidelis von Sigmaringen aus dem Kapuzinerorden, Erstlingsmärtyrers der Hl. Kongregation für Glaubensverbreitung
+        'did:op:73c511711d6ad19794cd3797149e3a9fbd6d615246ae2be8d56938985b715ed4', // Cross Asia Text
+        'did:op:fca47f74bd99d3a3c523bc3242497df4a098ceb028940428db18200c26e74995', // Cross Asia Algo
+        'did:op:ee381eb15d25d27b663565984601699473afeba4ba2efa43d9b6f9c88827f625', // XAsia Prob Data
+        'did:op:a63864d02fbda42fa945eb071093bfd69e2b9de2e083382028c531d468996875' // XAsia Prob Algo
       ]
     }
   ],
